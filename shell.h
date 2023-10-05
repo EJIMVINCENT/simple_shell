0dc912355ddbfa4aaadb54a55f1d048f428bf1e3
EJIMVINCENT <EJIMOVC@GMAIL.COM>
2023-10-05 10:13:12 -0700

    Update of function definitons in [shell.h]

--- shell.h
+++ shell.h
@@ -7,6 +7,28 @@
 
 
 
+/* string functions [str_func] */
 
+int _strlen(char *string);
+int _strcmp(char *s1, char *s2);
+int _strdup(char *str);
+char *_strcat(char *dest, char *src);
+char *strcpy(char *dest, char *src);
+
+
+
+/* print functions [print_func] */
+
+int putString(char *string);
+
+
+/* prompt function */
+
+void printPrompt();
+void getPrompt(char *input, int *count);
+
+/* execute functions */
+
+void exeCom(char **commands);
 
 #endif /* SHELL_H */
