526b4cc022122c7a5758fef79a5c91fdcbb15b9d
root <root@EJIMOVIC>
2023-10-21 02:58:31 +0100

    updating getline

--- readUInput_func.c
+++ readUInput_func.c
@@ -19,59 +19,6 @@ char *readLine(int *check)
 	return (input);
 }
 
-/**
- * _getline - custome getline function (reads input from
- * stdin)
- *
- * @input: destination to store characters read
- * @n: size of characters to read from stdin
- * @inputStream: inputStream to read from (stdin)
- *
- * Return: returns the number of character read
- *
- */
-
-int _getline(char **input, size_t *n, FILE *inputStream)
-{
-	ssize_t temp;
-	int status;
-	static ssize_t count;
-	char character = 'a';
-	char *buffer;
-
-	if (count == 0)
-		fflush(inputStream);
-	else
-		return (-1);
-	count = 0;
-	buffer = malloc(sizeof(char) * BUFFERSIZE);
-	if (buffer == NULL)
-		return (-1);
-	while (character != '\n')
-	{
-		status = read(STDIN_FILENO, &character, 1);
-		/*check for error and eof*/
-		if (status == -1 || (status == 0 && count == 0))
-		{
-			free(buffer);
-			return (-1);
-		}
-		if (status == 0 && count != 0)
-		{
-			count++;
-			break;
-		}
-		if (count >=  BUFFERSIZE)
-			buffer = _realloc(buffer, count, count + 1);
-		buffer[count] = character;
-		count++;
-	}
-	buffer[count] = '\0', *input = buffer, *n = count;
-	temp = count;
-	if (status != 0)
-		count = 0;
-	return (temp);
-}
 
 /**
  * remComment - removes comment from input
